d6b8c343ec330b1616e866582aa8945f36f98532
Abhishek Kumar <thehypnoticsilence!@gmail.com>
2025-02-05 17:12:17 +0530

    Hyperparameter training done

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,41 @@
 import os
 import sys
 
-import dill
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomExceptionHandler
 
-from sklearn.metrics import r2_score
-
 def save_object(file_path, obj):
     try:
-        dir_path =  os.path.dirname(file_path)
-        
+        dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
+        with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomExceptionHandler(e, sys)
-
-def evaluate_model(x_train, y_train, x_test, y_test, models):
+    
+def evaluate_model(x_train, y_train, x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(x_train)
 
@@ -39,7 +47,7 @@ def evaluate_model(x_train, y_train, x_test, y_test, models):
 
             report[list(models.keys())[i]] = test_model_score
 
-            return report
-        
+        return report
+
     except Exception as e:
-        raise CustomExceptionHandler(e,sys)
+        raise CustomExceptionHandler(e, sys)
