445ead1338855012d6448101126d2434523433eb
Abhishek Kumar <thehypnoticsilence!@gmail.com>
2025-02-05 17:39:31 +0530

    AWS deployement

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import pickle
 import numpy as np 
 import pandas as pd
 import dill
@@ -48,6 +49,14 @@ def evaluate_model(x_train, y_train, x_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomExceptionHandler(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomExceptionHandler(e, sys)
